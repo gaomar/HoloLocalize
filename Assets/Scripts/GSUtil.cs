232654165e3b28a78784c84083f135d5dab37793
h-takauma <h-takauma@i-enter.co.jp>
2017-09-15 10:57:31 +0900

    ・初回コミット

---
+++ Assets/Scripts/GSUtil.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GSUtil : MonoBehaviour {
+
+    private static GSUtil _Instance;
+
+    public bool isJapan = false;
+    public TextMesh helloText;
+
+    public static GSUtil Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                _Instance = FindObjectOfType<GSUtil>();
+            }
+            return _Instance;
+        }
+    }
+
+    private void Awake()
+    {
+        // タイムゾーン取得
+#if UNITY_UWP
+        TimeZoneInfo timezone = TimeZoneInfo.Local;
+        if (timezone.Id.Equals("Tokyo Standard Time"))
+        {
+            isJapan = true;
+        }
+        else
+        {
+            isJapan = false;
+        }
+#endif
+        // ローカライズファイル初期化
+        TextLocalizer.Init(isJapan);
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        helloText.text = TextLocalizer.Get("Hello");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TextLocalizer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class TextLocalizer {
+
+    // 文字列格納、検索用ディクショナリー
+    private static Dictionary<string, string> sDictionary = new Dictionary<string, string>();
+
+    /// <summary>
+    /// 文字列初期化
+    /// </summary>
+    /// <param name="isJapan">日本語かどうか</param>
+    public static void Init(bool isJapan)
+    {
+        // リソースファイルパス決定
+        string filePath;
+        if (isJapan)
+        {
+            filePath = "Text/japanese";
+        }
+        else
+        {
+            filePath = "Text/english";
+        }
+
+        // ディクショナリー初期化
+        sDictionary.Clear();
+        TextAsset csv = Resources.Load<TextAsset>(filePath);
+        StringReader reader = new StringReader(csv.text);
+        while (reader.Peek() > -1)
+        {
+            string[] values = reader.ReadLine().Split('\t');
+            sDictionary.Add(values[0], values[1].Replace("\\n", "\n"));
+        }
+    }
+
+    /// <summary>
+    /// 文字列取得
+    /// </summary>
+    /// <param name="key">文字列取得キー</param>
+    /// <returns>キーに該当する文字列</returns>
+    public static string Get(string key)
+    {
+        return sDictionary[key];
+    }
+
+}
